ba26bcc5d829074142053cea2bc86a6177a0bd83
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-04-15 08:23:29 +0300

    Update mk_cheat.cpp

--- mk_cheat.cpp
+++ mk_cheat.cpp
@@ -5,26 +5,40 @@
 #include <stdio.h>
 #include <opencv2/imgcodecs.hpp>
 #include <string>
+#include <iostream>
 using namespace cv;
 int main(int argc, char* argv[])
 {
 	IplImage* src = 0, * dst = 0;
-	for (int i = 1000; i <= 1003; i++)
+	for (int i = 1000; i <= 10000; i++)
 	{
+		String mm = "C://Users/almaz/source/repos/mk_cheat/resources/captcha/";
 		String png = ".png";
 		// имя картинки задаётся первым параметром
-		String filenames =  std::to_string(i) + png;
+		String filenames = mm + std::to_string(i) + png;
 		char filename[20];
-		strcpy(filename, filenames.c_str());
-		// получаем картинку
-		src = cvLoadImage(filename, 0);
-		printf("[i] image: %s\n", filename);
-		assert(src != 0);
-		dst = cvCreateImage(cvSize(src->width, src->height), IPL_DEPTH_8U, 1);
-		cvInRangeS(src, cvScalar(50), cvScalar(255), dst);
-		// показываем результаты
-		Mat m = cvarrToMat(dst);
-		cv::imwrite("opencv.jpg", m);
+		
+			strcpy(filename, filenames.c_str());
+			// получаем картинку
+			src = cvLoadImage(filename, 0);
+			
+			printf("[i] image: %s\n", filename);
+			if (src == 0)
+			{
+				std::cout << filenames << "dont exist";
+			}
+			else {
+			
+			dst = cvCreateImage(cvSize(src->width, src->height), IPL_DEPTH_8U, 1);
+			cvInRangeS(src, cvScalar(50), cvScalar(255), dst);
+			// показываем результаты
+			Mat m = cvarrToMat(dst);
+			String pp = "C://Users/almaz/source/repos/mk_cheat/resources/captcha_bin/";
+			char exitfile[100];
+			String fileexs = pp + std::to_string(i) + png;
+			strcpy(exitfile, fileexs.c_str());
+			cv::imwrite(exitfile, m);
+		}
 	}
 	return 0;
 }
